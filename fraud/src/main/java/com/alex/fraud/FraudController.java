268dcaa84a6b8906eaec95344b2288a9fc2907a9
AlexandruMBaltazar <alexandru.baltazar16@gmail.com>
2022-05-26 14:25:11 +0100

    Add sleuth and zipkin for tracing the distributed system

--- fraud/src/main/java/com/alex/fraud/FraudController.java
+++ fraud/src/main/java/com/alex/fraud/FraudController.java
@@ -1,9 +1,11 @@
 package com.alex.fraud;
 
 import com.alex.clients.fraud.FraudCheckResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/fraud-check")
 public record FraudController(FraudCheckService fraudCheckService) {
@@ -11,6 +13,8 @@ public record FraudController(FraudCheckService fraudCheckService) {
     public ResponseEntity<FraudCheckResponse> isFraudster(@PathVariable Long customerId) {
         boolean isFraudulentCustomer = fraudCheckService
                 .isFraudulentCustomer(customerId);
+
+        log.info("fraud check request for customer {}", customerId);
 
         return ResponseEntity.ok(new FraudCheckResponse(isFraudulentCustomer));
     }
